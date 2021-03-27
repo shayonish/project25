99f3c37df9589e61b196a07a2e6708dfb48a8aef
shayonish <69904313+shayonish@users.noreply.github.com>
2021-03-27 17:38:29 +0530

    Update Ground.js

--- MatterJSBoilerPlate-master/Ground.js
+++ MatterJSBoilerPlate-master/Ground.js
@@ -1,22 +1,22 @@
 class Ground {
-    constructor(x, y, width, height) {
-      var options = {
-        isStatic: true,
-        'restitution':0,
-        'friction':0,
-        'density':1
-      }
-      this.body = Bodies.rectangle(x, y, width, height, options);
-      this.width = width;
-      this.height = height;
-        
-      World.add(world, this.body);
-      }
-    display(){
-      rectMode(CENTER);
-  
-      fill(255);
-  
-      rect(this.body.position.x, this.body.position.y, this.width, this. height);
-      }
-    };  
+    constructor(x,y,width,height)   {
+        var options = {
+           isStatic:true 
+        }
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width;
+    this.height = height;
+    this.image = loadImage('base.png');
+    World.add(world,this.body);
+}
+
+    display()   {
+        push();
+        var pos1 = this.body.position;
+        translate(pos1.x,pos1.y);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop();
+    }
+
+}
