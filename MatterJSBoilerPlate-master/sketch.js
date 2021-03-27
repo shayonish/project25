6aa55f4400c01eaa1e4ef732bb8f9e6758c724b4
shayonish <69904313+shayonish@users.noreply.github.com>
2021-03-27 17:40:54 +0530

    Update sketch.js

--- MatterJSBoilerPlate-master/sketch.js
+++ MatterJSBoilerPlate-master/sketch.js
@@ -3,50 +3,56 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var engine, world;
+var ground1,box1,box2,box3,paper1
+
+function preload()
+{
+	boximage = loadImage('box.png');
+}
 
 function setup() {
-	createCanvas(800, 700);
-
-
+	
+	
+	var canvas = createCanvas(1400, 700);
+	rectMode(CENTER);
 	engine = Engine.create();
 	world = engine.world;
+	
 
-	//Create the Bodies Here.
-	paper = new Paper(100, 600, 10);
 
-  ground = new Ground(400, 680, 800, 20);
-  
-  leftSide = new Dustbin(550, 620, 20, 100);
-  bottom = new Dustbin(610, 660, 100, 20);
-  rightSide = new Dustbin(670, 620, 20, 100);
-
-	Engine.run(engine);
+	
+	box1 = new Box(1100,495,150,15);
+	box2 = new Box(1185,420,15,170);
+	box3 = new Box(1015,420,15,170);
+	
+	ground1 = new Ground(700,550,1400,30);
+	
+	paper1 = new Paper(200,300);
+	
+	 Engine.run(engine);
   
 }
 
 
 function draw() {
-  rectMode(CENTER);
-
-  background(0);
-  
-  Engine.update(engine);
-
-  paper.display();
-
-  ground.display();
-
-  leftSide.display();
-  bottom.display();
-  rightSide.display();
-
-  drawSprites();
+	background(206,237,227);
+	Engine.update(engine);
+	
+	
+	
+	ground1.display();
+	box3.display();
+	box1.display();
+	box2.display();
+	image(boximage,1000,330,200,200);
+	paper1.display();
+	
  
 }
 
-function keyPressed(){
-  if(keyCode === UP_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, {x:15, y: -15})
-  }
-}
+	function keyPressed()	{
+		if(keyCode===UP_ARROW)	{
+			Matter.Body.applyForce(paper1.body,paper1.body.position,{x:110,y:-110});
+
+		}
+	}
