4b4f77126ee758ccd0f7206873709f62770388da
shayonish <69904313+shayonish@users.noreply.github.com>
2021-03-27 17:39:15 +0530

    Update Paper.js

--- MatterJSBoilerPlate-master/Paper.js
+++ MatterJSBoilerPlate-master/Paper.js
@@ -1,21 +1,25 @@
-class Paper {
-    constructor(x, y, radius) {
-      var options = {
-        isStatic: false,
-        'restitution':0.3,
-        'friction':0.5,
-        'density':1.2
-      }
-      this.body = Bodies.circle(x, y, radius, options);
-      this.radius = radius
-        
-      World.add(world, this.body);
-      }
-    display(){
-      ellipseMode(RADIUS);
-  
-      fill("yellow");
-  
-      circle(this.body.position.x, this.body.position.y, this.radius);
+class Paper  {
+    constructor(x,y)   {
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            density: 1.2,
+            friction: 0.5,
+        }
+        this.body = Bodies.circle(x,y,23,options);
+    
+        this.image = loadImage('paper.png');
+        World.add(world,this.body);
+
     }
-    };
+
+        display()   {
+            push();
+            var pos = this.body.position;
+            translate(pos.x,pos.y);
+            imageMode(CENTER);
+            image(this.image,0,0,70,70);
+            pop();
+        }
+
+}
